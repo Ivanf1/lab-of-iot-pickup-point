0bdb89edd28186e6e1dc56345ccbfddc9b30ea8a
Ivan Capobianco <capobianco.ivan@gmail.com>
2024-02-19 15:51:20 +0100

    fix: cube insert request and response

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <ArduinoJson.h>
 #include <PubSubClient.h>
 #include <WiFi.h>
 #include <WiFiClient.h>
@@ -9,6 +10,8 @@
 #include <esp_log.h>
 
 #define TAG "MAIN"
+
+static const int PICKUP_POINT_N = 0;
 
 static const int LEFT_DROPPER_POSITION = 0;
 static const int RIGHT_DROPPER_POSITION = 1;
@@ -27,13 +30,11 @@ static const int rightGreenLedPin = 32;
 
 static const int WAIT_TIME_BEFORE_RECONNECT = 1000;
 
-static const char* release_cube_0_topic = "sm_iot_lab/pickup_point/0/cube/0/release";
-static const char* insert_request_cube_0_topic = "sm_iot_lab/pickup_point/0/cube/0/insert_request";
-static const char* insert_response_cube_0_topic = "sm_iot_lab/pickup_point/0/cube/0/inserted";
+static const char* release_cube_0_topic = "sm_iot_lab/pickup_point/0/cube/0/release_request";
+static const char* release_cube_1_topic = "sm_iot_lab/pickup_point/0/cube/1/release_request";
 
-static const char* release_cube_1_topic = "sm_iot_lab/pickup_point/0/cube/1/release";
-static const char* insert_request_cube_1_topic = "sm_iot_lab/pickup_point/0/cube/1/insert_request";
-static const char* insert_response_cube_1_topic = "sm_iot_lab/pickup_point/0/cube/1/inserted";
+static const char* insert_request_cube_topic = "sm_iot_lab/pickup_point/0/cube/insert_request";
+static const char* insert_response_cube_topic = "sm_iot_lab/pickup_point/0/cube/insert_response";
 
 Dropper leftDropper;
 Dropper rightDropper;
@@ -70,39 +71,79 @@ void on_mqtt_message_received(char* topic, byte* payload, unsigned int length) {
     return;
   }
 
-  if (strcmp(topic, insert_request_cube_0_topic) == 0) {
-    if (!leftDropper.isEmpty()) {
-      ESP_LOGE(TAG, "Cube dropper 0 is already occupied!");
+  if (strcmp(topic, insert_request_cube_topic) == 0) {
+    StaticJsonDocument<200> doc;
+    size_t doc_size;
+    uint8_t* output;
+    doc["pickupPointN"] = PICKUP_POINT_N;
+    doc["cubeId"] = (char*)payload;
+
+    if (leftDropper.isEmpty()) {
+      doc["cubeDropperN"] = LEFT_DROPPER_POSITION;
+      // wait for cube to be inserted
+      bool inserted = leftDropper.cubeInsertionRequest();
+      if (inserted) {
+        ESP_LOGD(TAG, "Cube has been inserted in left dropper");
+        doc["status"] = "inserted";
+
+        doc_size = measureJson(doc);
+        output = (uint8_t*)malloc(doc_size);
+        serializeJson(doc, (void*)output, doc_size);
+
+        mqttClient.publish(insert_response_cube_topic, output, doc_size);
+        free(output);
+      } else {
+        ESP_LOGD(TAG, "Cube could NOT be inserted in left dropper");
+        doc["status"] = "error";
+
+        doc_size = measureJson(doc);
+        output = (uint8_t*)malloc(doc_size);
+        serializeJson(doc, (void*)output, doc_size);
+
+        mqttClient.publish(insert_response_cube_topic, output, doc_size);
+        free(output);
+      }
       return;
     }
 
-    // wait for cube to be inserted
-    bool inserted = leftDropper.cubeInsertionRequest();
-    if (inserted) {
-      ESP_LOGD(TAG, "Cube 0 has been inserted");
-      mqttClient.publish(insert_response_cube_0_topic, "inserted");
-    } else {
-      ESP_LOGD(TAG, "Cube 0 has NOT been inserted");
-      mqttClient.publish(insert_response_cube_0_topic, "error");
-    }
-    return;
-  }
+    if (rightDropper.isEmpty()) {
+      doc["cubeDropperN"] = RIGHT_DROPPER_POSITION;
+      // wait for cube to be inserted
+      bool inserted = rightDropper.cubeInsertionRequest();
+      if (inserted) {
+        ESP_LOGD(TAG, "Cube has been inserted in right dropper");
+        doc["status"] = "inserted";
 
-  if (strcmp(topic, insert_request_cube_1_topic) == 0) {
-    if (!rightDropper.isEmpty()) {
-      ESP_LOGE(TAG, "Cube dropper 1 is already occupied!");
+        doc_size = measureJson(doc);
+        output = (uint8_t*)malloc(doc_size);
+        serializeJson(doc, (void*)output, doc_size);
+
+        mqttClient.publish(insert_response_cube_topic, output, doc_size);
+        free(output);
+      } else {
+        ESP_LOGD(TAG, "Cube could NOT be inserted in right dropper");
+        doc["status"] = "error";
+
+        doc_size = measureJson(doc);
+        output = (uint8_t*)malloc(doc_size);
+        serializeJson(doc, (void*)output, doc_size);
+
+        mqttClient.publish(insert_response_cube_topic, output, doc_size);
+        free(output);
+      }
       return;
     }
 
-    // wait for cube to be inserted
-    bool inserted = rightDropper.cubeInsertionRequest();
-    if (inserted) {
-      ESP_LOGD(TAG, "Cube 1 has been inserted");
-      mqttClient.publish(insert_response_cube_1_topic, "inserted");
-    } else {
-      ESP_LOGD(TAG, "Cube 1 has NOT been inserted");
-      mqttClient.publish(insert_response_cube_1_topic, "error");
-    }
+    ESP_LOGE(TAG, "Cube Droppers are all already full!");
+
+    doc["cubeDropperN"] = -1;
+    doc["status"] = "error";
+    doc_size = measureJson(doc);
+    output = (uint8_t*)malloc(doc_size);
+    serializeJson(doc, (void*)output, doc_size);
+
+    mqttClient.publish(insert_response_cube_topic, output, doc_size);
+    free(output);
     return;
   }
 }
@@ -113,9 +154,8 @@ void mqtt_connect() {
     if (mqttClient.connect("pickup-point-0")) {
       ESP_LOGD(TAG, "MQTT connection established");
       mqttClient.subscribe(release_cube_0_topic);
-      mqttClient.subscribe(insert_request_cube_0_topic);
       mqttClient.subscribe(release_cube_1_topic);
-      mqttClient.subscribe(insert_request_cube_1_topic);
+      mqttClient.subscribe(insert_request_cube_topic);
     } else {
       ESP_LOGD(TAG, "MQTT connection failed. Try again in %d seconds", WAIT_TIME_BEFORE_RECONNECT / 1000);
       delay(WAIT_TIME_BEFORE_RECONNECT);
